e0f54e3d693f0ab934f18d24e5fe00acc8c589d0
Ernest Avagovich <erni1937@gmail.com>
2022-08-11 19:26:42 +0300

    Создал проект для прохождения собеседования на обучение

---
+++ Space_X_Project/Space_X_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Space_X_Project
+//
+//  Created by Ernest Avagovich on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
